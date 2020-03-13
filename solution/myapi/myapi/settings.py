1ee9b8d077c349337279ef5afe3142eff2c00dde
lucaslopes0198 <lucaslopes0198@gmail.com>
2020-03-12 23:26:08 -0300

    Fix allowed_hosts and update .gitignore

--- solution/myapi/myapi/settings.py
+++ solution/myapi/myapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['btp-place-api.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bcp-places-api.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
